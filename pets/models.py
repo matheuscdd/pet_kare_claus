f077e0b78614b5fe77bb339a4fde3796145f40e9
matheuscdd <matheuscdd@hotmail.com>
2023-04-04 16:11:38 -0300

    feat tarefa 1 finalizada

--- pets/models.py
+++ pets/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class SexPet(models.TextChoices):
+    MALE = 'Male'
+    FEMALE = 'Female'
+    DEFAULT = 'Not Informed'
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    weight = models.FloatField()
+    sex = models.CharField(
+        max_length=20,
+        default=SexPet.DEFAULT,
+        choices=SexPet.choices
+    )
+    group = models.ForeignKey('groups.Group', on_delete=models.PROTECT, related_name='pets')
